f8140e6c6ae4c0440d36e4e4a78401f4b170c0f8
5060vishalkumar <5060vishalkumar@gmail.com>
2024-02-24 07:48:00 +0530

    update account js

--- src/Account.js
+++ src/Account.js
@@ -1,49 +1,126 @@
-import {Alert, Button, StyleSheet, Text, View,ScrollView,Image,TouchableOpacity} from 'react-native';
-import React from 'react';
+import {
+  Alert,
+  Button,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+  Switch,
+} from 'react-native';
+import React, {useState} from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+// import ProfileEditForm from './ProfileEditForm';
 
 const Account = ({navigation}) => {
+  const [isEnabled, setIsEnabled] = useState(false);
+  // const [isEditFormOpen, setIsEditFormOpen] = useState(false);
+  const toggleSwitch = () => setIsEnabled(previousState => !previousState);
+
   const logout = async () => {
     await AsyncStorage.removeItem('userId');
     Alert.alert('Logout Done !');
     navigation.navigate('Login');
   };
   return (
-    <View>
-      <ScrollView>
-      <View className="flex flex-col justify-around  px-8 flex-1">
-        <View>
-          <Image
-            source={require('../assets/images/bu_logo.png')}
-            className="flex m-auto w-28 h-28 mt-5"
-          />
+    <View className="bg-white flex-1">
+      {/* {isEditFormOpen && (
+        <ProfileEditForm close={() => {
+          setIsEditFormOpen(false)
+        }} />
+      )} */}
+      <Text className="text-center font-bold text-violet-600 text-lg my-2">
+        BUNDEL U
+      </Text>
+      <View className="relative flex-row justify-center my-2">
+        {/* <Image
+          className="absolute w-[72px] h-[72px] top-10 right-[130px] z-50"
+          source={require('./../assets/images/edit.png')}
+        /> */}
+        <Image
+          className="flex w-24 h-24 "
+          style={{
+            borderWidth: 4,
+            borderColor: 'rgb(153, 0, 255)',
+            borderRadius: 100, // Border width
+          }}
+          source={require('../assets/images/bu_logo.png')}
+        />
+      </View>
 
-          <Text className="m-auto text-3xl text-primary font-Regular my-5">
-            BU<Text className="text-black">NDEL U</Text>
+      <View
+        className="flex bg-white shadow-2xl shadow-[#b5b5b5]  mx-5 p-4 my-[10px] rounded-md"
+        style={styles.card}>
+        <View>
+          <Text className="flex-row text-lg text-black mb-3 font-semibold">
+            Personal Info
           </Text>
         </View>
-
-      {/* <Button title="Logout" onPress={logout} /> */}
-        <View className="my-10">
-          {/* <TextInput
-            className=" border-gray border rounded my-2 px-4 font-Regular text-xs py-2"
-            placeholder="Enter your email Address"
-            keyboardType="email-address"
-            value={email}
-            onChangeText={setemail}
-          /> */}
-
+        <View className="flex gap-5 p-2">
+          <View className="flex-row justify-between">
+            <Text className="text-gray ">Your Name</Text>
+            <Text className="text-black">Siddhant Raj</Text>
+          </View>
+          <View className="flex-row justify-between">
+            <Text className="text-gray ">Education Level</Text>
+            <Text className="text-black">Bachelor</Text>
+          </View>
+          <View className="flex-row justify-between">
+            <Text className="text-gray ">Address</Text>
+            <Text className="text-black">Jhansi, Uttar Pradesh</Text>
+          </View>
+        </View>
+      </View>
+      <View
+        className="flex bg-white shadow-2xl shadow-[#b5b5b5]  mx-5 p-4 my-[10px] rounded-md"
+        style={styles.card}>
+        <View>
+          <Text className="flex-row text-lg text-black mb-3 font-semibold">
+            Contact Info
+          </Text>
+        </View>
+        <View className="flex gap-5 p-2">
+          <View className="flex-row justify-between">
+            <Text className="text-gray ">Phone number</Text>
+            <Text className="text-black">+91 8303109832</Text>
+          </View>
+          <View className="flex-row justify-between">
+            <Text className="text-gray ">Email</Text>
+            <Text className="text-black">rajsiddhant284@gmail.com</Text>
+          </View>
           <TouchableOpacity
-            className="bg-primary  rounded-md py-3 my-5"
-            onPress={logout}>
-            <Text className="text-sm text-center  text-white font-Regular">
-              Logout
-            </Text>
+            className="p-4 rounded-md bg-purple-600"
+            // onPress={() => {
+            //   setIsEditFormOpen(true);
+            // }}
+          >
+            <Text className="text-center text-white font-semibold">Edit</Text>
           </TouchableOpacity>
         </View>
       </View>
-    </ScrollView>
-  
+      <View
+        className="flex bg-white shadow-2xl shadow-[#b5b5b5]  mx-5 p-4 my-[10px] rounded-md"
+        style={styles.card}>
+        <View>
+          <Text className="flex-row text-lg text-black mb-3 font-semibold">
+            Personal Info
+          </Text>
+        </View>
+        <View className="flex gap-4 p-2">
+          <View className="flex-row justify-between">
+            <Text className="text-gray ">Email Notification</Text>
+            <Switch
+              trackColor={{false: '#767577', true: 'rgb(124, 58, 237) '}}
+              thumbColor={isEnabled ? 'white' : 'white'}
+              onValueChange={toggleSwitch}
+              value={isEnabled}
+            />
+          </View>
+          <View className="flex-row justify-between">
+            <Text className="text-red-600 text-lg">Logout</Text>
+          </View>
+        </View>
+      </View>
     </View>
   );
 };
